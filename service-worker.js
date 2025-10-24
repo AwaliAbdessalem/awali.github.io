584c4bdc146e25e59510614c130dfc554ca7d677
Abdessalem Awali <31621206+AwaliAbdessalem@users.noreply.github.com>
2025-10-24 01:53:54 +0100

    Implement service worker with caching strategy
    Signed-off-by: Abdessalem Awali <31621206+AwaliAbdessalem@users.noreply.github.com>

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'kedma-blogger-v1';
+const urlsToCache = [
+  '/',
+  '/?m=1'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
